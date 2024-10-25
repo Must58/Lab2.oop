cce896c03a3bd38d84e0a9ed0bb17bf1ed6ebc5c
Must58 <baghdadm21@gmail.com>
2024-10-25 17:09:02 +0300

    Add project files.

---
+++ Lab2.oop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.oop
+{
+
+    class Vehicle
+    {
+        public string brand;
+    }
+    class Motorvehicle : Vehicle
+    {
+        public string modelName;
+    }
+    class Car : Motorvehicle
+    {
+
+    }
+    class Truck : Motorvehicle
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.brand = "Mercedes";
+            c.modelName = "E300";
+            Console.WriteLine("The brand of Car: " + c.brand);
+            Console.WriteLine("The model Name of Car: " + c.modelName);
+            Truck t = new Truck();
+            c.brand = "Scania";
+            c.modelName = "770s";
+            Console.WriteLine("The brand of Truck: " + c.brand);
+            Console.WriteLine("The model Name of Truck: " + c.modelName);
+            Console.ReadLine();
+        }
+}}
